f8c2bcfdd72c7c97e9cbd256f145c3043b040189
yuli <114781331@qq.com>
2020-09-01 18:28:47 +0800

    测试配置修改

--- src/test/java/com/cjs/example/CjsSpringsecurityExampleApplicationTests.java
+++ src/test/java/com/cjs/example/CjsSpringsecurityExampleApplicationTests.java
@@ -3,6 +3,7 @@ package com.cjs.example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +12,24 @@ public class CjsSpringsecurityExampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void encode() {
+		BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+		String hashPass = bcryptPasswordEncoder.encode("123456");
+
+		System.out.println(hashPass);
+
+		boolean f = bcryptPasswordEncoder.matches("123456", hashPass);
+		System.out.println(f);
+
+		hashPass = bcryptPasswordEncoder.encode("123456");
+
+		System.out.println(hashPass);
+
+		f = bcryptPasswordEncoder.matches("123456", hashPass);
+		System.out.println(f);
 	}
 
 }
